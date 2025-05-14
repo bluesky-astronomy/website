f5750d5ba064e7bd04e663cdd4e9f7dc66507b48
emilyhunt <emily.hunt.physics@gmail.com>
2025-05-14 16:33:26 +0200

    Little tweak so I can query localhost

--- src/lib/js/flask-api.js
+++ src/lib/js/flask-api.js
@@ -1,4 +1,5 @@
 export async function getFeedList() {
-	const response = await fetch('https://feed-all.astronomy.blue/api/app.getFeedList');
+	// const response = await fetch('https://feed-all.astronomy.blue/api/app.getFeedList');
+	const response = await fetch('http://127.0.0.1:8000/api/app.getFeedList');
 	return await response.json();
 }
