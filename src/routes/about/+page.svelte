aed7d6f7a485d1dc7f1c18838cbfe8e2fa3b4014
Emily Hunt <emily.hunt.physics@gmail.com>
2025-05-18 16:44:22 +0200

    Basic about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,16 @@
 <h1>About us</h1>
 
-<p>Todo.</p>
+<p>
+	Since its founding in July 2023, <em>The Astrosky Ecosystem</em> has grown into a large open-source
+	project with multiple volunteer developers and twelve volunteer moderators.
+</p>
+
+<p>
+	We're proud to host feeds with nearly two thousand signed up users - including international space
+	agencies and billion-dollar megatelescope projects.
+</p>
+
+<p>
+	Most of all, we're excited for the future: a future in which science communication and discussion
+	spaces can become safe, stable, and permanent spaces.
+</p>
