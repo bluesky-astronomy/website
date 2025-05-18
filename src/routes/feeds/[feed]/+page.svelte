1685b87ff9202b94e3664a6ed1a4d976b021d1ec
Emily Hunt <emily.hunt.physics@gmail.com>
2025-05-18 16:44:44 +0200

    Working, basic one-feed info page

--- src/routes/feeds/[feed]/+page.svelte
+++ src/routes/feeds/[feed]/+page.svelte
@@ -1,13 +1,48 @@
 <!-- svelte-ignore non_reactive_update -->
 <script>
+	import { getFeedInfo } from '$lib/js/cache.svelte';
+	import { onMount } from 'svelte';
 	import Image from '$lib/blocks/Image.svelte';
-	import { siteTitle } from '$lib/config.js';
-	import { formatDate } from '$lib/js/dates';
+	import ImageWithText from '$lib/blocks/ImageWithText.svelte';
+	// import { siteTitle } from '$lib/config.js';
+	// import { formatDate } from '$lib/js/dates';
 	let { data } = $props();
 
 	const feed = data.feed;
+	let feedInfo = $state(0);
+
+	onMount(async () => {
+		const allFeedInfo = await getFeedInfo();
+		feedInfo = allFeedInfo.filter((thisFeed) => thisFeed.feed === feed)[0];
+		console.log(feedInfo);
+	});
 </script>
 
-<h1>The {String(feed[0]).toUpperCase() + String(feed).slice(1)} Feed</h1>
+{#if feedInfo}
+	<Image
+		src={feedInfo.avatar}
+		style="width: 100px; border-radius: 35px; margin-top: 20px; margin-bottom: 0px"
+	/>
+	<h1 style="margin-top: 10px">The {feedInfo.displayName.replace('The', '')} Feed</h1>
 
-<p>Contains all posts containing TODO</p>
+	<!-- <h2>About the feed</h2> -->
+	<p><strong>{feedInfo.description.split('.')[0].split('!')[0]}.</strong></p>
+
+	<!-- <h2>How to post to the feed</h2> -->
+
+	<p>
+		Like all of our feeds, the {feedInfo.displayName.replace('The', '')} feed is
+		<strong>opt-in.</strong> Your posts will only be able to appear here after
+		<a href="/about/signup">signing up</a>.
+	</p>
+
+	<p>
+		After that, you can post to the feed by including <strong
+			>{feedInfo.emoji.concat(feedInfo.words).join(', ')}</strong
+		> in your post.
+	</p>
+
+	
+{:else}
+	<p>Loading...</p>
+{/if}
