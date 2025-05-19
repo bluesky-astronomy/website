fa0025cd30bd94d19ff5fdd5070d5c186e91fbdc
Emily Hunt <emily.hunt.physics@gmail.com>
2025-05-19 21:44:04 +0200

    Tidy up page

--- src/routes/feeds/+page.svelte
+++ src/routes/feeds/+page.svelte
@@ -20,26 +20,6 @@
 
 {#if sortedFeeds}
 	{#each sortedFeeds as feed}
-		<FeedCard feed={feed.feed}/>
-		<!-- <a href={`/feeds/${feed.feed}`}>
-			<div class="feed-card">
-				<h3 style="margin-bottom: 10px">{feed.displayName}</h3>
-				{feed.words}
-				<p style="color: var(--color-grey); margin-top: 0px">
-					{feed.emoji.concat(feed.words)}<br />❤️{feed.likeCount}
-				</p>
-			</div>
-		</a> -->
+		<FeedCard feed={feed.feed} />
 	{/each}
 {/if}
-
-<style>
-	.feed-card {
-		background-color: var(--color-offwhite);
-		padding: 0px 10px 0px 10px;
-		margin-top: 10px;
-		margin-bottom: 10px;
-		width: 50%;
-		min-width: 450px;
-	}
-</style>
