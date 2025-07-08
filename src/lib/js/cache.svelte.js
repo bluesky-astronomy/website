e88a8ebfae798c5236a52383ecc9ea1b83cee68f
emilyhunt <emily.hunt.physics@gmail.com>
2025-07-08 18:28:18 +0200

    Simplify async getFeedInfo further (it's actually basically sync now too)

--- src/lib/js/cache.svelte.js
+++ src/lib/js/cache.svelte.js
@@ -4,14 +4,9 @@ import { getActorFeeds } from './bsky-api';
 import { getFeedList } from './flask-api';
 import { feeds } from '../assets/json/feedInfo.json';
 
-let feedInfo = $state({});
-
 export async function getFeedInfo() {
-	if (Object.keys(feedInfo).length > 0) {
-		return feedInfo;
-	}
-	feedInfo = feeds;
-	return feedInfo;
+	// Todo: refactor to remove
+	return feeds;
 }
 
 export function getFeedInfoSync() {
