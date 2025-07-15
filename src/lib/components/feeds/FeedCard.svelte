c8433a2b63ef38c08d9df978033759e52b24c98e
Emily Hunt <emily.hunt.physics@gmail.com>
2025-07-15 17:52:49 +0200

    Refactor to just use static info

--- src/lib/components/feeds/FeedCard.svelte
+++ src/lib/components/feeds/FeedCard.svelte
@@ -1,37 +1,32 @@
 <script>
 	import { onMount } from 'svelte';
-	import { getFeedInfo } from '$lib/js/cache.svelte';
+	import { getFeedInfoSync } from '$lib/js/cache.svelte';
 	import { numberFormatter } from '$lib/js/format';
 	let { feed } = $props();
 
-	let feedInfo = $state(0);
-
-	onMount(async () => {
-		const allFeedInfo = await getFeedInfo();
-		feedInfo = allFeedInfo[feed];
-	});
+	const feedInfo = getFeedInfoSync()[feed];
 </script>
 
 <div class="card">
 	<a href="/feeds/{feed}">
-        {#if feedInfo}
-            <div class="container">
-                <img class="feed-avatar" src={feedInfo.avatar} alt="{feedInfo.displayName}'s avatar" />
-                <!-- <div style="display: flex; align-items: center; margin-left: 10px; height: 100px"> -->
-                <h3 style="margin-left: 10px; margin-bottom: 0px; margin-top: 18px; padding: 0px;">
-                    {feedInfo.displayName}
-                </h3>
-                <!-- </div> -->
-            </div>
-            <p style="margin-top: 10px; margin-bottom: 0px">
-                {feedInfo.description.split('.')[0].split('!')[0]}.
-            </p>
-            <p style="margin-top: 5px; color: var(--color-grey)">
-                Liked by {numberFormatter.format(feedInfo.likeCount)} users
-            </p>
-        {:else}
+        <!-- {#if feedInfo} -->
+		<div class="container">
+			<img class="feed-avatar" src={feedInfo.avatar} alt="{feedInfo.displayName}'s avatar" />
+			<!-- <div style="display: flex; align-items: center; margin-left: 10px; height: 100px"> -->
+			<h3 style="margin-left: 10px; margin-bottom: 0px; margin-top: 18px; padding: 0px;">
+				{feedInfo.displayName}
+			</h3>
+			<!-- </div> -->
+		</div>
+		<p style="margin-top: 10px; margin-bottom: 0px">
+			{feedInfo.description.split('.')[0].split('!')[0]}.
+		</p>
+		<p style="margin-top: 5px; color: var(--color-grey)">
+			Liked by {numberFormatter.format(feedInfo.likeCount)} users
+		</p>
+        <!-- {:else}
             <p>Loading...</p>
-        {/if}
+        {/if} -->
     </a>
 </div>
 
